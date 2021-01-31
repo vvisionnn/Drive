782e65fa9d9fb4b12ec3caf1b025e82c276c210d
vvisionnn <wz546500392@gmail.com>
2021-01-31 19:04:48 +0800

    update row style and add some icons

--- src/components/filemanager/ItemsList.tsx
+++ src/components/filemanager/ItemsList.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 import {List, ListItem, Typography, ListItemAvatar, Avatar, ListItemText, IconButton, ListItemSecondaryAction} from "@material-ui/core";
-import FolderIcon from "@material-ui/icons/Folder"
 import InfoRoundedIcon from '@material-ui/icons/InfoRounded';
 import GetAppRoundedIcon from '@material-ui/icons/GetAppRounded';
+import FolderRoundedIcon from '@material-ui/icons/FolderRounded';
+import InsertDriveFileRoundedIcon from '@material-ui/icons/InsertDriveFileRounded';
+
+declare interface fileProp {
+  hashes: {quickXorHash: string},
+  mimeType: string,
+}
+
+declare interface folderProp {
+  childCount: number
+}
 
 export declare interface itemProp {
   name: string,
   createdDateTime: Date,
-  folder: { childCount: number },
+  folder?: folderProp,
+  file?: fileProp
   id: string,
   size: number,
   webUrl: string,
@@ -27,7 +38,7 @@ export default function ItemsList(props: ItemsListProps) {
         <ListItem button disableRipple key={index} >
           <ListItemAvatar>
             <Avatar>
-              <FolderIcon />
+              {item.folder ? <FolderRoundedIcon /> : <InsertDriveFileRoundedIcon />}
             </Avatar>
           </ListItemAvatar>
           <ListItemText onClick={() => updateHandler(item)}>
@@ -35,9 +46,9 @@ export default function ItemsList(props: ItemsListProps) {
               {item.name}
             </Typography>
           </ListItemText>
-          <IconButton>
+          {item.file && <IconButton>
             <GetAppRoundedIcon />
-          </IconButton>
+          </IconButton>}
           <IconButton>
             <InfoRoundedIcon />
           </IconButton>
