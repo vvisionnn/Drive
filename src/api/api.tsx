1ecac58b9872061aab7a24ee58ce18c287dd52dd
vvisionnn <wz546500392@gmail.com>
2021-01-31 18:03:39 +0800

    extract api as a single file

---
+++ src/api/api.tsx
@@ -0,0 +1,34 @@
+import {useState} from "react";
+import axios from "axios";
+
+const api = {
+  fetchStatus: () => {
+    return axios.get("/api/auth/stat")
+  }
+}
+
+const useStatusApi = () => {
+  const [status, setStatus] = useState<boolean>(false)
+  const [statusLoading, setStatusLoading] = useState<boolean>(false)
+  const [isFetchStatusErr, setIsFetchStatusErr] = useState<boolean>(false)
+
+  const doFetchStatus = () => {
+    setStatusLoading(true)
+    api.fetchStatus().then(resp => {
+      console.log(resp)
+      setStatus(resp
+        && resp.data
+        && resp.data['status']
+        && resp.data['status'] === "ok")
+    }).catch(err => {
+      console.log(`fetch login status error: ${err}`)
+      setIsFetchStatusErr(true)
+    }).finally(() => {
+      setStatusLoading(false)
+    })
+  }
+
+  return {status, statusLoading, isFetchStatusErr, doFetchStatus};
+};
+
+export {useStatusApi}
